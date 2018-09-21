7046095a6c6e625b11298cd1e63b258285ee86aa
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-09-21 03:30:35 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,12 +22,6 @@ RSpec.configure do |config|
        :headers => {'Accept'=>'application/json'}).
        to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
 
-      #  stub_request(:post, "https://github.com/login/oauth/access_token").
-      #  with(:body => {"client_id"=>"", "client_secret"=>"", "code"=>"20"},
-      #       :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.9.1'}).
-      #  to_return(:status => 200, :body => "", :headers => {})
-
-
     stub_request(:get, "https://api.github.com/user").
       with(:headers => {'Authorization'=>'token 1'}).
       to_return(:status => 200, :body => {"login"=>"your_username"}.to_json, :headers => {})
