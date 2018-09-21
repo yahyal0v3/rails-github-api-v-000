b62032a83ca15cd9c692d21a9ac063091e03ce56
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-09-21 03:21:12 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require 'capybara/rspec'
 require 'webmock/rspec'
 require 'rack_session_access/capybara'
 
+
 RSpec.configure do |config|
   config.include Capybara::DSL
 
@@ -17,9 +18,15 @@ RSpec.configure do |config|
       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
 
     stub_request(:post, "https://github.com/login/oauth/access_token").
-      with(:body => {"client_id"=> ENV["GITHUB_CLIENT_ID"], "client_secret"=> ENV["GITHUB_CLIENT_SECRET"], "code"=>"20"},
-      :headers => {'Accept'=>'application/json'}).
-      to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
+       with(:body => {"client_id"=> "", "client_secret"=> "", "code"=>"20"},
+       :headers => {'Accept'=>'application/json'}).
+       to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
+
+      #  stub_request(:post, "https://github.com/login/oauth/access_token").
+      #  with(:body => {"client_id"=>"", "client_secret"=>"", "code"=>"20"},
+      #       :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Faraday v0.9.1'}).
+      #  to_return(:status => 200, :body => "", :headers => {})
+
 
     stub_request(:get, "https://api.github.com/user").
       with(:headers => {'Authorization'=>'token 1'}).
